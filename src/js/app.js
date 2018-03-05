f0fbefe5bac9e1444d73d8d570c231a0cfd6b546
guerilla7 <root@rdelrosa-mbpc.local>
2018-03-05 09:34:03 -0800

    modified: src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -24,18 +24,30 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+    // If no injected web3 instance is detected, fall back to Ganache
+    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+  }
+  web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function(data) {
+    // Get the necessary contract artifact file and instantiate it with truffle-contract
+    var AdoptionArtifact = data;
+    App.contracts.Adoption = TruffleContract(AdoptionArtifact);
 
+    // Set the provider for our contract
+    App.contracts.Adoption.setProvider(App.web3Provider);
+
+    // Use our contract to retrieve and mark the adopted pets
+    return App.markAdopted();
+  });
     return App.bindEvents();
   },
 
